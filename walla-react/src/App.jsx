745d94ea9295cde9935b15cc8cd021032ea40519
Saul Dominguez <saul.vau@gmail.com>
2024-05-04 08:50:43 +0200

    routes links y routes anidados acabados (en principio), añadido componente protectRoutes para proteger las direcciones cuando no se esta logeado

--- walla-react/src/App.jsx
+++ walla-react/src/App.jsx
@@ -1,16 +1,34 @@
 import AdsPage from "./adsPage/adsPage.jsx";
 import LoginPage from "./loginPage/loginPage.jsx";
 
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import NewAdsPage from "./newAdsPage/newAdsPage.jsx";
+import AdDetailPage from "./adsPage/componentes/adDetailPage.jsx";
+import AuthRoute from "./authorize/components/ProtectRoutes.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/adverts" element={<AdsPage />} />
+      <Route
+        path="/adverts"
+        element={
+          <AuthRoute>
+            <div className="outlet-container">
+              <Outlet />
+            </div>
+          </AuthRoute>
+        }
+      >
+        <Route index element={<AdsPage />} />
+        <Route path=":id" element={<AdDetailPage />} />
+        <Route path="new" element={<NewAdsPage />} />
+      </Route>
+
       <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/adverts/new" element={<NewAdsPage />} />
+
+      <Route path="*" element={<Navigate to="404" />} />
+      <Route path="/404" element={<div>404 Not found</div>} />
     </Routes>
   );
 }
